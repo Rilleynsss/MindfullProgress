3ae9296b7dd2065103f08b9818a34b77595c0505
rilleyns <616tm616@gmail.com>
2024-04-01 22:19:32 +0300

    clear hook

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -4,13 +4,9 @@ import { TaskSlice } from "../store/reducers/TaskSlice";
 import { ITask } from "../models/ITask";
 
 export const useTimer = () => {
-  const dispatch = useAppDispathc();
   const { task } = useAppSelector((state) => state.task);
   const [idx, setIdx] = useState<number>(-1);
   const [currentTask, setCurrentTask] = useState<ITask>();
-  // if (idx) {
-  //   setCurrentTask(task[idx]);
-  // }
   const { setIsFinished } = TaskSlice.actions;
   const [isStarted, setIsStarted] = useState<boolean>(false);
 
@@ -21,14 +17,6 @@ export const useTimer = () => {
       // dispatch(setIsFinished(idx));
     }, 1000);
   }, [task]);
-
-  // const [timer, setTimer] = useState({
-  //   hour: Math.floor(time / 60),
-  //   minute: time % 60,
-  // });
-  // console.log(currentTask);
-  if (isStarted) {
-  }
 
   return { setIsStarted, setIdx } as const;
 };
